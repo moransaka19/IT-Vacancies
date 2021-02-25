52be66bb73138a4f81746fe080411334c41c2101
moransaka19 <moransaka19@gmail.com>
2021-02-25 19:28:38 +0200

    v1.0.2 fix files

--- Domain/Vacancy.cs
+++ Domain/Vacancy.cs
@@ -7,7 +7,9 @@ namespace Domain
         public string Name { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
-        public int CompanyId { get; set; }
-        public Company Company { get; set; }
+        public string RecrutierName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string Location { get; set; }
     }
 }
